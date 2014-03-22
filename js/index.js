31e5c4e23909d1b12f9f1a8a7edcaddd6b198c8c
Rudy Lehman <rudylehman@gmail.com>
2014-03-22 15:04:28 -0700

    test

--- js/index.js
+++ js/index.js
@@ -100,24 +100,9 @@ var app = {
             }
         );
     },
-    flashlightOn: function(){
+    vibrate: function(){
 
-        window.plugins.flashlight.toggle();
+        navigator.notification.vibrate(2500);
 
-//        window.plugins.flashlight.available(function(isAvailable) {
-//            if (isAvailable) {
-//
-//                // switch on
-//                window.plugins.flashlight.switchOn(); // success/error callbacks may be passed
-//
-//                // switch off after 3 seconds
-//                setTimeout(function() {
-//                    window.plugins.flashlight.switchOff(); // success/error callbacks may be passed
-//                }, 3000);
-//
-//            } else {
-//                alert("Flashlight not available on this device");
-//            }
-//        });
     }
 };
