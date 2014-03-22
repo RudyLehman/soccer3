4931fcba77d0c5e4e7d1dbc8a84699696701e967
Rudy Lehman <rudylehman@gmail.com>
2014-03-22 11:53:11 -0700

    test

--- js/index.js
+++ js/index.js
@@ -68,16 +68,26 @@ var app = {
     },
 
     scanBarcode: function() {
-        navigator.barcodeScanner.scan(
-            function (result) {
+
+        window.plugins.barcodeScanner.scan( function(result) {
                 alert("We got a barcode\n" +
                     "Result: " + result.text + "\n" +
                     "Format: " + result.format + "\n" +
                     "Cancelled: " + result.cancelled);
-            },
-            function (error) {
+            }, function(error) {
                 alert("Scanning failed: " + error);
             }
         );
+//        cordova.plugins.barcodeScanner.scan(
+//            function (result) {
+//                alert("We got a barcode\n" +
+//                    "Result: " + result.text + "\n" +
+//                    "Format: " + result.format + "\n" +
+//                    "Cancelled: " + result.cancelled);
+//            },
+//            function (error) {
+//                alert("Scanning failed: " + error);
+//            }
+//        );
     }
 };
