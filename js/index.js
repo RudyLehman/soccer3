f6bc0637afcbb3a0d00046b9cc227dc4f78c8e73
Rudy Lehman <rudylehman@gmail.com>
2014-03-22 14:55:25 -0700

    test

--- js/index.js
+++ js/index.js
@@ -99,5 +99,22 @@ var app = {
                 alert("Scanning failed: " + error);
             }
         );
+    },
+    flashlightOn: function(){
+        window.plugins.flashlight.available(function(isAvailable) {
+            if (isAvailable) {
+
+                // switch on
+                window.plugins.flashlight.switchOn(); // success/error callbacks may be passed
+
+                // switch off after 3 seconds
+                setTimeout(function() {
+                    window.plugins.flashlight.switchOff(); // success/error callbacks may be passed
+                }, 3000);
+
+            } else {
+                alert("Flashlight not available on this device");
+            }
+        });
     }
 };
